0d344e926aa6fde6ddd43e9f02f5c27c0f1a8921
sajid <sajidshaikh12543@gmail.com>
2024-06-22 21:03:22 +0530

    20-06-2024

--- lib/screen/add/add_category_screen.dart
+++ lib/screen/add/add_category_screen.dart
@@ -128,7 +128,7 @@ Padding CustomTextField({controller,hint,kboard}) {
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
           label: Text("Enter $hint",style: TextStyle(color: Color(0xff015B8A)),),
           focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: Color(0xff015B8A),width: 1.5),borderRadius: BorderRadius.circular(10)),
-          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0x550A1172),width: 1),borderRadius: BorderRadius.circular(10)),
+          enabledBorder: OutlineInputBorder(borderSide: const BorderSide(color: Color(0x550A1172),width: 1),borderRadius: BorderRadius.circular(10)),
           enabled: true
 
       ),
