a4f704280d7aa39ba407bd12d75b93b56573feae
sajid <sajidshaikh12543@gmail.com>
2024-06-26 14:15:12 +0530

    20-06-2024

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:amazing_quotes/controller/quote_controller.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'package:sizer/sizer.dart';
 
 import '../model/quote_model.dart';
