a43c79217e4c6e85b4cd54f8ddb8b3daa91632f3
sajid <sajidshaikh12543@gmail.com>
2024-08-28 22:28:15 +0530

    28-08-2024

--- lib/screen/view_fliterQuote_Screen.dart
+++ lib/screen/view_fliterQuote_Screen.dart
@@ -12,235 +12,298 @@ class View_FilteredQuote_Screen extends StatefulWidget {
   const View_FilteredQuote_Screen({super.key});
 
   @override
-  State<View_FilteredQuote_Screen> createState() => _View_FilteredQuote_ScreenState();
+  State<View_FilteredQuote_Screen> createState() =>
+      _View_FilteredQuote_ScreenState();
 }
 
 class _View_FilteredQuote_ScreenState extends State<View_FilteredQuote_Screen> {
-
   TextEditingController tquote = TextEditingController();
-
 
   int tempImgIndex = 3;
   String title = "";
   String subdata = "";
-  String what  = "";
+  String what = "";
   Map mapDATA = {};
   QuoteController control = Get.put(QuoteController());
+
   @override
   void initState() {
     super.initState();
 
     mapDATA = Get.arguments;
-    what  = mapDATA['what'];
-    if(what  == "category")
-    {
+    what = mapDATA['what'];
+    if (what == "category") {
       subdata = "category";
-    }
-    else
-    {
+    } else {
       subdata = "author";
     }
 
     title = mapDATA["value"];
-
-
   }
 
   @override
   Widget build(BuildContext context) {
-
     return SafeArea(
-
       child: Scaffold(
-
           appBar: AppBar(
               backgroundColor: const Color(0xff015B8A),
               elevation: 0,
               centerTitle: true,
-              title: Text(title,style: TextStyle(fontSize: 20.sp,fontWeight: FontWeight.w600),)
-          ),
-
+              title: Text(
+                title,
+                style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w600),
+              )),
           body: Obx(
-                () =>  ListView.builder(
-              padding: EdgeInsets.only(left:3.w,right:3.w,top: 5.w),
+            () => ListView.builder(
+              padding: EdgeInsets.only(left: 3.w, right: 3.w, top: 5.w),
               itemCount: control.filterData.length,
               itemBuilder: (context, index) {
-
                 print("\n view screen ==== == ${control.filterData.value}");
                 Random r = Random();
                 control.imgIndex.value = r.nextInt(control.bgImgList.length);
 
                 return GestureDetector(
-
                   onTap: () {
-                    tquote = TextEditingController(text: control.filterData[index]['quote']);
-                    Get.defaultDialog(title: "Update Quote",content: Column(
-                      children: [
-                        SizedBox(height: 1.5.h,),
-                        CustomTextField(hint: "Quote",kboard: TextInputType.text,controller: tquote),
-                        SizedBox(height: 2.h,),
-                        Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    tquote = TextEditingController(
+                        text: control.filterData[index]['quote']);
+                    Get.defaultDialog(
+                        title: "Update Quote",
+                        content: Column(
                           children: [
-                            IconButton(onPressed: () async {
+                            SizedBox(
+                              height: 1.5.h,
+                            ),
+                            CustomTextField(
+                                hint: "Quote",
+                                kboard: TextInputType.text,
+                                controller: tquote),
+                            SizedBox(
+                              height: 2.h,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                IconButton(
+                                  onPressed: () async {
+                                    Quote_DB_Helper.quote_db_helper
+                                        .deleteInQuoteTABLE(
+                                            control.filterData[index]['id']);
 
-                              Quote_DB_Helper.quote_db_helper.deleteInQuoteTABLE(control.filterData[index]['id']);
+                                    what == "category"
+                                        ? control
+                                            .filterQuotesAccordingToCategory(
+                                                title)
+                                        : control.filterQuotesAccordingToAuthor(
+                                            title);
 
-                              what == "category"
-                                  ? control.filterQuotesAccordingToCategory(title)
-                                  : control.filterQuotesAccordingToAuthor(title);
+                                    control.loadCategoryDB();
+                                    Get.back();
+                                  },
+                                  icon: const Icon(Icons.delete_outline_rounded),
+                                  iconSize: 30.sp,
+                                  color: Colors.red,
+                                ),
+                                IconButton(
+                                  onPressed: () async {
+                                    QuoteModel model = QuoteModel(
+                                        author: control.filterData[index]
+                                            ['author'],
+                                        category: control.filterData[index]
+                                            ['category'],
+                                        fav: control.filterData[index]['fav'],
+                                        id: control.filterData[index]['id'],
+                                        quote: tquote.text);
 
-                              control.loadCategoryDB();
-                              Get.back();
+                                    await Quote_DB_Helper.quote_db_helper
+                                        .updateInQuoteTABLE(model);
 
-                            }, icon: Icon(Icons.delete_outline_rounded),iconSize: 30.sp,color: Colors.red,),
+                                    what == "category"
+                                        ? control
+                                            .filterQuotesAccordingToCategory(
+                                                title)
+                                        : control.filterQuotesAccordingToAuthor(
+                                            title);
 
-
-                            IconButton(onPressed: () async {
-
-                              QuoteModel model = QuoteModel(
-                                  author: control.filterData[index]['author'],
-                                  category: control.filterData[index]['category'],
-                                  fav: control.filterData[index]['fav'],
-                                  id: control.filterData[index]['id'],
-                                  quote: tquote.text
-                              );
-
-                              await Quote_DB_Helper.quote_db_helper.updateInQuoteTABLE(model);
-
-                              what == "category"
-                                  ? control.filterQuotesAccordingToCategory(title)
-                                  : control.filterQuotesAccordingToAuthor(title);
-
-                              control.loadCategoryDB();
-                              tquote.clear();
-                              Get.back();
-
-                            }, icon: Icon(Icons.check),iconSize: 30.sp,color: Colors.green,)
+                                    control.loadCategoryDB();
+                                    tquote.clear();
+                                    Get.back();
+                                  },
+                                  icon: const Icon(Icons.check),
+                                  iconSize: 30.sp,
+                                  color: Colors.green,
+                                )
+                              ],
+                            )
                           ],
-                        )
-                      ],
-                    ));
+                        ));
                   },
                   child: Container(
-                    margin: EdgeInsets.only(bottom:3.w),
+                    margin: EdgeInsets.only(bottom: 3.w),
                     height: 25.h,
                     width: 100.w,
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(5.w),
-                        image: DecorationImage(image: AssetImage("${control.bgImgList[control.imgIndex.value]}"),fit: BoxFit.cover),
-
-                        border: Border.all(color: Colors.black12)
-                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.w),
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "${control.bgImgList[control.imgIndex.value]}"),
+                            fit: BoxFit.cover),
+                        border: Border.all(color: Colors.black12)),
                     child: Column(
                       children: [
-                        SizedBox(height: 3.h,),
-                        Text("${control.filterData[index]['quote']}",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 13.sp,color: Colors.white),maxLines: 5,overflow: TextOverflow.ellipsis,textAlign: TextAlign.center,),
-                        Spacer(),
-                        Row(mainAxisAlignment: MainAxisAlignment.end,
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        Text(
+                          "${control.filterData[index]['quote']}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 13.sp,
+                              color: Colors.white),
+                          maxLines: 5,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                        ),
+                        const Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
                           children: [
-                            Text(what == "category"
-                                ? "- ${control.filterData[index]['author']}"
-                                : "- ${control.filterData[index]['category']}"
-                              ,style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15.sp,color: Colors.white),textAlign: TextAlign.center,),
-                            SizedBox(width: 5.w,),
+                            Text(
+                              what == "category"
+                                  ? "- ${control.filterData[index]['author']}"
+                                  : "- ${control.filterData[index]['category']}",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15.sp,
+                                  color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              width: 5.w,
+                            ),
                           ],
                         ),
-                        SizedBox(height: 1.5.h,),
-
-                        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        SizedBox(
+                          height: 1.5.h,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           mainAxisSize: MainAxisSize.min,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            IconButton(icon: Icon(Icons.done_all_outlined,color: Colors.white),onPressed: () {
-
-                            },),
-                            IconButton(icon: Icon(Icons.copy_outlined,color: Colors.white),
+                            IconButton(
+                              icon: const Icon(Icons.done_all_outlined,
+                                  color: Colors.white),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.copy_outlined,
+                                  color: Colors.white),
                               onPressed: () {
-
-                                if(control.imgIndex.value < control.bgImgList.length - 1)
-                                {
+                                if (control.imgIndex.value <
+                                    control.bgImgList.length - 1) {
                                   tempImgIndex = control.imgIndex.value++;
-                                }
-                                else
-                                {
+                                } else {
                                   control.imgIndex.value = 0;
                                 }
-                                print("img Index ==========${control.imgIndex.value}");
-                              },),
-                            IconButton(icon: Icon(Icons.favorite_outline_rounded,color: Colors.white),onPressed: () {
-
-                            },),
+                                print(
+                                    "img Index ==========${control.imgIndex.value}");
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.favorite_outline_rounded,
+                                  color: Colors.white),
+                              onPressed: () {},
+                            ),
                           ],
                         ),
-                        SizedBox(height: 1.h,)
+                        SizedBox(
+                          height: 1.h,
+                        )
                       ],
                     ),
                   ),
-
-
-
-
                 );
-              },),
-          )
-
-
-      ),
+              },
+            ),
+          )),
     );
   }
 
-  Widget QuoteListTile({quote,author,img})
-  {
+  Widget QuoteListTile({quote, author, img}) {
     return Container(
-      margin: EdgeInsets.only(bottom:3.w),
+      margin: EdgeInsets.only(bottom: 3.w),
       height: 25.h,
       width: 100.w,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(5.w),
-          image: DecorationImage(image: AssetImage("$img"),fit: BoxFit.cover),
-
-          border: Border.all(color: Colors.black12)
-      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.w),
+          image: DecorationImage(image: AssetImage("$img"), fit: BoxFit.cover),
+          border: Border.all(color: Colors.black12)),
       child: Column(
         children: [
-          SizedBox(height: 3.h,),
-          Text("$quote",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 13.sp,color: Colors.white),maxLines: 5,overflow: TextOverflow.ellipsis,textAlign: TextAlign.center,),
-          Spacer(),
-          Row(mainAxisAlignment: MainAxisAlignment.end,
+          SizedBox(
+            height: 3.h,
+          ),
+          Text(
+            "$quote",
+            style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 13.sp,
+                color: Colors.white),
+            maxLines: 5,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Text("- $author",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15.sp,color: Colors.white),textAlign: TextAlign.center,),
-              SizedBox(width: 5.w,),
+              Text(
+                "- $author",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15.sp,
+                    color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                width: 5.w,
+              ),
             ],
           ),
-          SizedBox(height: 1.5.h,),
-
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          SizedBox(
+            height: 1.5.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              IconButton(icon: Icon(Icons.done_all_outlined,color: Colors.white),onPressed: () {
-
-              },),
-              IconButton(icon: Icon(Icons.copy_outlined,color: Colors.white),
+              IconButton(
+                icon: const Icon(Icons.done_all_outlined, color: Colors.white),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.copy_outlined, color: Colors.white),
                 onPressed: () {
-
-                  if(control.imgIndex.value < control.bgImgList.length - 1)
-                  {
+                  if (control.imgIndex.value < control.bgImgList.length - 1) {
                     tempImgIndex = control.imgIndex.value++;
-                  }
-                  else
-                  {
+                  } else {
                     control.imgIndex.value = 0;
                   }
                   print("img Index ==========${control.imgIndex.value}");
-                },),
-              IconButton(icon: Icon(Icons.favorite_outline_rounded,color: Colors.white),onPressed: () {
-
-              },),
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.favorite_outline_rounded, color: Colors.white),
+                onPressed: () {},
+              ),
             ],
           ),
-          SizedBox(height: 1.h,)
+          SizedBox(
+            height: 1.h,
+          )
         ],
       ),
     );
   }
-
 }
