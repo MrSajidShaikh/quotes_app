c574a4c8a1dc81c8c3f72c0a9987918efcf0d6e4
sajid <sajidshaikh12543@gmail.com>
2024-08-29 21:37:03 +0530

    28-08-2024

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,16 @@ import 'screen/splash_Screen.dart';
 void main() {
   runApp(
     Sizer(
-      builder: (context, orientation, deviceType) => GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        routes: {
-          "/": (p0) => const SplashScreen(),
-          "/home": (p0) => const HomeScreen(),
-          "/add": (p0) => const Add_Screen(),
-          "/viewCategory": (p0) => const View_FilteredQuote_Screen(),
-        },
-      ),
+      builder: (context, orientation, deviceType) =>
+          GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            routes: {
+              "/": (p0) => const SplashScreen(),
+              "/home": (p0) => const HomeScreen(),
+              "/add": (p0) => const Add_Screen(),
+              "/viewCategory": (p0) => const View_FilteredQuote_Screen(),
+            },
+          ),
     ),
   );
 }
